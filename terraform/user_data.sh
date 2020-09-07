36f4a07f14dfb8ad7eb46fc1e7110e499f061ca8
Sofia Demyanovska <sofia.demyanovska@gmail.com>
2020-09-07 16:34:59 +0300

    first commit

---
+++ terraform/user_data.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+sudo yum update -y
+sudo amazon-linux-extras install docker -y
+sudo service docker start
+
+sudo docker pull sofdem/back
+
+# Run docker container
+sudo docker run --name backend -p 5000:80 sofdem/back
